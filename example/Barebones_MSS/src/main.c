27a48711a33892af1517bc7ce8383f7535886a52
Victor Hogeweij <hogeweyv@gmail.com>
2024-11-29 01:15:49 +0100

    -add comments

--- example/Barebones_MSS/src/main.c
+++ example/Barebones_MSS/src/main.c
@@ -1,3 +1,28 @@
+/*
+*  Copyright 2024 (C) Jeroen Veen <ducroq> & Victor Hogeweij <Hoog-V>
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Author:          Victor Hogeweij <Hoog-V>
+*
+*  This file is part of the IWR6xxx_Toolchain project
+*/
+/*
+*  Barebones MSS example, prints "Hello World!" over the Control UART
+*  and demonstrates how CMake should be set-up.
+*
+*/
+
 #include <sys_common.h>
 #include <soc.h>
 #include <pinmux.h>
